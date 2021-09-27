7dfef17ab058cc61e3e499f96df9fe64e9ac5a4a
eternal <eternal.liu@outlook.com>
2021-09-27 15:03:26 +0800

    自定义RedisTemplate调整

--- RedisConfig.java
+++ RedisConfig.java
@@ -14,6 +14,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
     @Bean
+    @SuppressWarnings("all")
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
